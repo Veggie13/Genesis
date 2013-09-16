8b883d259619f1b643d3411e9ce7a8c0b5af600c
Veggie <cmderochie+github@gmail.com>
2013-09-15 22:24:52 -0600

    Fixed logic in Array Resize.

--- Common/Tools/Extensions.cs
+++ Common/Tools/Extensions.cs
@@ -25,8 +25,8 @@ namespace Genesis.Common.Tools
         public static T[,] Resize<T>(this T[,] original, int x, int y)
         {
             T[,] newArray = new T[x, y];
-            int minX = Math.Min(original.GetLength(0), newArray.GetLength(0));
-            int minY = Math.Min(original.GetLength(1), newArray.GetLength(1));
+            int minY = Math.Min(original.GetLength(0), newArray.GetLength(0));
+            int minX = Math.Min(original.GetLength(1), newArray.GetLength(1));
 
             for (int i = 0; i < minY; ++i)
                 Array.Copy(original, i * original.GetLength(0), newArray, i * newArray.GetLength(0), minX);
